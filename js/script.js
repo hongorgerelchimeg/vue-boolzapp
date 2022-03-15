bf65a702950e0d21e3b6432a180e1cbf3c8064f5
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-15 17:10:39 +0100

    array list of contacts created

--- js/script.js
+++ js/script.js
@@ -2,7 +2,49 @@ const app = new Vue(settings =
    {
       el: '#root',
       data: {
-         name: '',
+         listaContatti: [
+            {
+               name: 'Michele',
+               avatar: 'avatar_1.jpg',
+               lastMsg: '12:00'
+            },
+            {
+               name: 'Fabio',
+               avatar: 'avatar_2.jpg',
+               lastMsg: '12:00'
+            },
+            {
+               name: 'Samuele',
+               avatar: 'avatar_3.jpg',
+               lastMsg: '12:00'
+            },
+            {
+               name: 'Alessandro B.',
+               avatar: 'avatar_4.jpg',
+               lastMsg: '12:00'
+            },
+            {
+               name: 'Alessandro L.',
+               avatar: 'avatar_5.jpg',
+               lastMsg: '12:00'
+            },
+            {
+               name: 'Claudia',
+               avatar: 'avatar_6.jpg',
+               lastMsg: '12:00'
+            },
+            {
+               name: 'Federico',
+               avatar: 'avatar_7.jpg',
+               lastMsg: '12:00'
+            },
+            {
+               name: 'Davide',
+               avatar: 'avatar_8.jpg',
+               lastMsg: '12:00'
+            }
+
+         ],
          saluto: '',
          
       },
